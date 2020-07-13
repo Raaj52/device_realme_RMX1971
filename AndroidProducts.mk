1d36b2058077873acb62035273e52da0debe210b
Raaj52 <mail2raaj52@gmail.com>
2020-07-13 10:15:27 +0000

    RMX1971: Let's bringup ZenX-OS!
    Signed-off-by: Raaj52 <mail2raaj52@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,13 @@
 #
-# Copyright (C) 2019 The LineageOS Project
+# Copyright (C) 2020 ZenX-OS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_RMX1971.mk
+    $(LOCAL_DIR)/zenx_RMX1971.mk
+
+COMMON_LUNCH_CHOICES := \
+    zenx_RMX1971-user \
+    zenx_RMX1971-userdebug \
+    zenx_RMX1971-eng
